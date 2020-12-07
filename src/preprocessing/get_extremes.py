7b60ca58b68100a46f01974be20b75077e76b9ed
Simon Thomas <simon@sdat2-mac.local>
2020-12-07 11:52:21 +0000

    worked out how many floods there are above 3.4m in 1990-2020 (7 pm 1)

--- src/preprocessing/get_extremes.py
+++ src/preprocessing/get_extremes.py
@@ -1,5 +1,7 @@
 """
 Script assumes that the data is in the src/data directory.
+
+python3 src/preprocessing/get_extremes.py
 """
 import os
 import numpy as np
@@ -10,32 +12,38 @@ import matplotlib.pyplot as plt
 from functools import wraps
 
 
-def get_wiski_data(prefix, resolution='60min', field='stage', fill_in_value=np.nan):
+def get_wiski_data(prefix, resolution="60min", field="stage", fill_in_value=np.nan):
 
-    print('get wiski data script starting (takes a long time)')
+    print("get wiski data script starting (takes a long time)")
 
-    file_name = {'60min': prefix + 'Shipston Wiski data - 60 min.xlsx',
-                 '15min': prefix +'Shipston Wiski data - 15 min.xlsx'}
+    file_name = {
+        "60min": prefix + "Shipston Wiski data - 60 min.xlsx",
+        "15min": prefix + "Shipston Wiski data - 15 min.xlsx",
+    }
 
-    sheet_name = {'flow': 1,
-                  'stage': 2,
-                  'rainfall': 3}
+    sheet_name = {"flow": 1, "stage": 2, "rainfall": 3}
 
-    field_name = {'rainfall': 'Precipitation [mm]',
-                  'stage': 'Stage [m]',
-                  'flow': 'Flow [m³/s]'}
+    field_name = {
+        "rainfall": "Precipitation [mm]",
+        "stage": "Stage [m]",
+        "flow": "Flow [m³/s]",
+    }
 
-    initial_dataframe = pd.read_excel(file_name[resolution], sheet_name=sheet_name[field],
-                                      parse_dates=[['Date', 'Time']])
+    initial_dataframe = pd.read_excel(
+        file_name[resolution],
+        sheet_name=sheet_name[field],
+        parse_dates=[["Date", "Time"]],
+    )
 
-    initial_dataframe['Date_Time'] = pd.to_datetime(
-        initial_dataframe['Date_Time'])
+    initial_dataframe["Date_Time"] = pd.to_datetime(initial_dataframe["Date_Time"])
 
-    filtered_dataframe = initial_dataframe.replace(
-        " ---", fill_in_value).replace("  ---", np.nan)
+    filtered_dataframe = initial_dataframe.replace(" ---", fill_in_value).replace(
+        "  ---", np.nan
+    )
 
     filtered_dataframe[field_name[field]] = pd.to_numeric(
-        filtered_dataframe[field_name[field]], downcast="float")
+        filtered_dataframe[field_name[field]], downcast="float"
+    )
 
     return filtered_dataframe
 
@@ -47,18 +55,20 @@ def get_peaks_above_threshold(df, key, threshold):
     npa = df[key].to_numpy()
     peaks, _ = find_peaks(npa, height=threshold)
 
-    return df.iloc[peaks.tolist(),]
+    print("threshold", threshold, "peaks", len(peaks))
+
+    return df.iloc[
+        peaks.tolist(),
+    ]
 
 
 if __name__ == "__main__":
-    
+
     this_path = os.path.realpath(__file__)
     PROJECT_PATH = os.path.dirname(os.path.dirname(this_path))
-    data_loc = os.path.join(PROJECT_PATH, "data") + '/'
+    data_loc = os.path.join(PROJECT_PATH, "data") + "/"
 
-    threshold = 2.7 # metres
-    stage60_filtered = get_wiski_data(data_loc)
-    df = get_peaks_above_threshold(stage60_filtered,
-                                   'Stage [m]', threshold)
-    df.to_excel(data_loc + 'stage_60min_'
-                + str(threshold) +'m_threshold.xlsx')
+    for threshold in [2.7, 3.0, 3.4]:
+        stage60_filtered = get_wiski_data(data_loc)
+        df = get_peaks_above_threshold(stage60_filtered, "Stage [m]", threshold)
+        df.to_excel(data_loc + "stage_60min_" + str(threshold) + "m_threshold.xlsx")
